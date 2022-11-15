d60c313021133049190bc6eae982a374196386b6
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-11-15 10:53:25 +0000

    fix portal www endpoint url for the catalog

--- role_provider_org_admin/03_catalog/create_catalog.sh
+++ role_provider_org_admin/03_catalog/create_catalog.sh
@@ -17,7 +17,7 @@ PORTAL_NAME=$(yq -r '.name' ${PORTAL_OBJ})
 
 export CONFIGURED_GATEWAY_SERVICE_URL=$(apic gateway-services:get -s $SERVER -o $P_ORG_NAME --availability-zone availability-zone-default --scope org $GATEWAY_NAME --format json --output - | jq -r '.url')
 export PORTAL_SERVICE_URL=$(apic portal-services:get -s $SERVER -o $P_ORG_NAME --availability-zone availability-zone-default --scope org $PORTAL_NAME --format json --output - | jq -r '.url')
-PORTAL_ENDPOINT_URL=$(apic portal-services:get -s $SERVER -o $P_ORG_NAME --availability-zone availability-zone-default --scope org $PORTAL_NAME --format json --output - | jq -r '.endpoint')
+PORTAL_WWW_URL=$(apic portal-services:get -s $SERVER -o $P_ORG_NAME --availability-zone availability-zone-default --scope org $PORTAL_NAME --format json --output - | jq -r '.web_endpoint_base')
 
 apic catalogs:create -s $SERVER -o $P_ORG_NAME $CATALOG_OBJ
 
@@ -28,7 +28,7 @@ CONFIGURED_GATEWAY_OBJ=/tmp/configured-gateway-service.yaml
 apic configured-gateway-services:create --scope catalog -s $SERVER -o $P_ORG_NAME -c $CATALOG_NAME $CONFIGURED_GATEWAY_OBJ
 
 CATALOG_SETTING_OBJ=${SCRIPT_ROOT}/objects/catalog-setting.yaml
-export CATALOG_ENDPOINT="${PORTAL_ENDPOINT_URL}/${P_ORG_NAME}/${NAME}"
+export CATALOG_ENDPOINT="${PORTAL_WWW_URL}/${P_ORG_NAME}/${NAME}"
 yglu $CATALOG_SETTING_OBJ > /tmp/catalog-setting.yaml
 CATALOG_SETTING_OBJ=/tmp/catalog-setting.yaml
 
